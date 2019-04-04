c46e273f067157bf80420f221cc91233400b5ae6
Costanza Volpini <costanza.volpini@gmail.com>
2019-04-04 17:00:03 +0200

    added structure folders

---
+++ generator_training_test.py
@@ -0,0 +1,13 @@
+import torch
+import math
+from torch import Tensor
+
+
+def generator(n):
+    input = Tensor(n, 2).uniform_(0, 1) #[0,1]^2
+    radius = 1/(2*math.pi) #0.398...
+    target = (input - 0.5).pow(2).sum(1).sub(radius).sign().add(1).div(2).long()
+    return input, target
+
+train_input, train_target = generator(1000)
+test_input, test_target = generator(1000)
