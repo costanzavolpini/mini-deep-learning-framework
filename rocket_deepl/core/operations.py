5590b121a3dd0010da101adbc832d11f053ca5ff
Francis Damachi <francis.damachi@epfl.ch>
2019-04-05 16:54:41 +0200

    set up mult /div

--- rocket_deepl/core/operations.py
+++ rocket_deepl/core/operations.py
@@ -21,3 +21,23 @@ class Sub(BinaryOperation):
     def backward(self, gradient):
         self.l.backward(gradient * 1)
         self.r.backward(gradient * (-1))
+
+
+class Div(BinaryOperation):
+    def forward(self, l, r):
+        super().forward(l, r)
+        return tensor.Tensor(l.data /r.data, prev_op = self)
+
+
+    def backward(self, gradient):
+        pass
+
+
+class Multiply(BinaryOperation):
+    def forward(self, l, r):
+        super().forward(l, r)
+        return tensor.Tensor(l.data *r.data, prev_op = self)
+
+
+    def backward(self, gradient):
+        pass
