4a710130b968745a9431e8dbf57f3534ea0d5e21
Costanza Volpini <costanza.volpini@gmail.com>
2019-04-11 21:11:35 +0200

    fixed

--- generator_training_test.py
+++ generator_training_test.py
@@ -2,6 +2,7 @@ import torch
 import math
 
 
+
 def generator(n, one_hot_enc = False):
     """
 
@@ -12,16 +13,15 @@ def generator(n, one_hot_enc = False):
     target = (input - 0.5).pow(2).sum(1).sub(radius).sign().add(1).div(2).long()
 
     if one_hot_enc == True:
-        #target shape would be 2x1
+
         target_hot = torch.empty(target.shape[0], 2)
         target_hot[:, :] = 0 # fill all with 0s
 
         # target in this case is an id than if target = 0, we put 1 in the first column, if target = 1 we put 1 in second column
-        target_hot[:, (1, 2, 3)] = 1
-        print(target_hot)
+        target_hot[:,0] = target == 0
+        target_hot[:,1] = target == 1
+
         return input, target_hot
 
     return input, target
 
-train_input, train_target = generator(1000, True)
-# test_input, test_target = generator(1000)
