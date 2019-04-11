154df6a90430a4c48873a539b0822a525303e283
Costanza Volpini <costanza.volpini@gmail.com>
2019-04-11 18:18:59 +0200

    Clean and add comments

--- rocket_deepl/core/activations/relu.py
+++ rocket_deepl/core/activations/relu.py
@@ -1,29 +1,27 @@
 import torch
 from rocket_deepl.module import *
 
-
 class  ReLU(Module):
+    """
+        Non-linear function used to guarantee a good accuracy of the model.
+        ReLU is half rectified (from bottom).
+        f(z) = 0 when z < 0 and f(z) = z when z >= 0.
+        range: [ 0 to infinity)
+
+        Issue: if z < 0 then f(z) = 0 that causes a bad fit or train of our data.
+    """
 
     def forward(self, input):
-
-        
-
 
         input[input < 0] = 0.0
 
         #input = input.t()
-
-
-
-
-
         return input
 
-    #TODO:
     def backward(self, gradientwrtoutput):
         """
-        Since relu is not differentiable in 0, we just split the two cases:
-        where the value > 0 the derivative is 1 else is 0. In 0 is not differentiable!
+        Since ReLU is not differentiable in 0, we just split the two cases:
+        where the value > 0 the derivative is 1 else is 0.
         """
         gradientwrtoutput[gradientwrtoutput > 0] = 1
         gradientwrtoutput[gradientwrtoutput < 0] = 0
--- rocket_deepl/core/losses/l_mse.py
+++ rocket_deepl/core/losses/l_mse.py
@@ -3,27 +3,20 @@ from rocket_deepl.module import *
 
 
 class MSEloss(Module):
+    """
+    TODO:
+    """
     def __call__(self, estimated, target):
         return self.forward(estimated, target)
 
     #TODO:
     def forward(self, estimated, target):
         self.estimated = estimated.t()
-
         self.target = target
-   
-
-
         loss_mse = ((estimated - target) ** 2).mean()
-
-
-
         return loss_mse
 
 
     #TODO:
     def backward(self, *noparam):
-        to_return =  2* (self.estimated - self.target)
-
-
-        return to_return
+        return 2 * (self.estimated - self.target)
