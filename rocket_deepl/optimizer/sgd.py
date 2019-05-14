0712d6efd762a35addb5735a38fe4f34220524cb
Costanza Volpini <costanza.volpini@gmail.com>
2019-05-14 16:12:48 +0200

    sgd commented

--- rocket_deepl/optimizer/sgd.py
+++ rocket_deepl/optimizer/sgd.py
@@ -2,10 +2,9 @@ import torch
 from rocket_deepl.module import *
 from rocket_deepl.core.layers import *
 
-
 class SGD():
     """
-    Stochastic gradient descent consists of updating the parameters w_t after every sample w_{t+1}
+    Stochastic gradient descent consists of updating the parameter:
     w_{t+1} = w_t - step * gradient of loss_{n(t)} (w_t)
     """
 
@@ -14,22 +13,21 @@ class SGD():
         Initialization function.
         Input:
             model: model
-            step: value
+            step: learning rate
         """
         self.model = model
-        self.step = step #step = learning rate
-
+        self.step = step
 
 
     def update_weight(self):
         """
-        Function to update all the parameters. #TODO: maybe rename into update_params?
+        Function to update all the parameters.
         self.weight = [ [w_l], [w_l+1], ... , [w_n] ]
         self.grad_loss = [ [gl_l], [gl_l+1], ... , [gl_n] ]
         """
-        for layer in self.model.modules :
+        for layer in self.model.modules:
+            # update parameters just in module that are of type Linear
             if(type(layer) is Linear):
-
                 layer.w = layer.w - (self.step * layer.grad_w)
                 layer.b = layer.b - (self.step * layer.grad_b)
 
